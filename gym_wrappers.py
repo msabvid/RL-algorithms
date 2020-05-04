8dc129faa567e6ac3ca9439890017cacec45a75d
Marc Sabate <s2001981@mathsdgx1.maths.ed.ac.uk>
2020-05-04 22:52:44 +0100

    wrappers

--- gym_wrappers.py
+++ gym_wrappers.py
@@ -1,6 +1,7 @@
 import gym
 import numpy as np
-
+import cv2
+from gym import spaces
 
 class ClipRewardEnv(gym.RewardWrapper):
     def __init__(self, env):
@@ -17,3 +18,67 @@ class ClipRewardEnv(gym.RewardWrapper):
         """
         return np.sign(reward)
 
+
+class ResizeFrameEnv(gym.ObservationWrapper):
+    def __init__(self, env):
+        """
+        Warp frames to 84x84 as done in the Nature paper and later work.
+        :param env: (Gym Environment) the environment
+        """
+        gym.ObservationWrapper.__init__(self, env)
+        self.width = 84
+        self.height = 84
+        self.observation_space = spaces.Box(
+            low=0, high=255, shape=(1, self.height, self.width),
+            dtype=env.observation_space.dtype)
+
+    def observation(self, frame):
+        """
+        returns the current observation from a frame
+        :param frame: ([int] or [float]) environment frame
+        :return: ([int] or [float]) the observation
+        """
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        frame = cv2.resize(
+            frame, (self.width, self.height), interpolation=cv2.INTER_AREA)
+        return frame[None, :, :]/255
+
+
+
+class EpisodicLifeEnv(gym.Wrapper):
+    def __init__(self, env):
+        """
+        Make end-of-life == end-of-episode, but only reset on true game over.
+        Done by DeepMind for the DQN and co. since it helps value estimation.
+        :param env: (Gym Environment) the environment to wrap
+        """
+        gym.Wrapper.__init__(self, env)
+        self.lives = 0
+        self.was_real_done = True
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.was_real_done = done
+        # check current lives, make loss of life terminal,
+        # then update lives to handle bonus lives
+        lives = self.env.unwrapped.ale.lives()
+        if 0 < lives < self.lives:
+            done = True
+        self.lives = lives
+        return obs, reward, done, info
+
+    def reset(self, **kwargs):
+        """
+        Calls the Gym environment reset, only when lives are exhausted.
+        This way all states are still reachable even though lives are episodic,
+        and the learner need not know about any of this behind-the-scenes.
+        :param kwargs: Extra keywords passed to env.reset() call
+        :return: ([int] or [float]) the first observation of the environment
+        """
+        if self.was_real_done:
+            obs = self.env.reset(**kwargs)
+        else:
+            # no-op step to advance from terminal/lost life state
+            obs, _, _, _ = self.env.step(0)
+        self.lives = self.env.unwrapped.ale.lives()
+        return obs
