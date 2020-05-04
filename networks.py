f0df7a22cea53e529ee96b99be5a920afacbed88
Marc Sabate <s2001981@mathsdgx1.maths.ed.ac.uk>
2020-05-04 22:52:31 +0100

    Nature conv net

--- networks.py
+++ networks.py
@@ -48,13 +48,34 @@ class ConvNet(nn.Module):
     Network from Nature paper of DeepMind: https://www.nature.com/articles/nature14236.pdf
     """
     def __init__(self, nChannels, nOut):
-
+        super().__init__()
         self.layer1 = nn.Sequential(nn.Conv2d(nChannels, 32, kernel_size=8, stride=4), nn.ReLU())
         self.layer2 = nn.Sequential(nn.Conv2d(32,64,kernel_size=4,stride=2), nn.ReLU())
         self.layer3 = nn.Sequential(nn.Conv2d(64,64,kernel_size=3, stride=1),nn.ReLU())
         self.layer4 = nn.Sequential(nn.Linear(7*7*64, 512), nn.ReLU())
         self.layer5 = nn.Linear(512, nOut)
 
+    def freeze(self):
+        for p in self.parameters():
+            p.requires_grad=False
+
+    def unfreeze(self):
+        for p in self.parameters():
+            p.requires_grad=True
+
+    def hard_update(self, source_net):
+        """Updates the network parameters by copying the parameters
+        of another network
+        """
+        for target_param, source_param in zip(self.parameters(), source_net.parameters()):
+            target_param.data.copy_(source_param.data)
+
+    def soft_update(self, source_net, tau):
+        """Updates the network parameters with a soft update by polyak averaging
+        """
+        for target_param, source_param in zip(self.parameters(), source_net.parameters()):
+            target_param.data.copy_((1-tau)*target_param.data + tau*source_param.data)
+    
     def forward(self, x):
         x = self.layer1(x)
         x = self.layer2(x)
